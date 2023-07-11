1265854e85aba4a624c0677453462cc2731b4e60
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-11 11:28:50 +0530

    [Riya] Add : Implemented Daily Emp Wage feature

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -8,15 +8,21 @@ namespace EmployeeWageComputation
 {
     public class EmployeeWage
     {
+        const int WAGE_PER_HR = 20, FULL_DAY_HR = 8;
         public void EmployeeAttendance()
         {
             Random random = new Random();
-            if empCheck = random.Next(0, 2);//returns value 0, 1
+            int empCheck = random.Next(0, 2);//returns value 0, 1
             if (empCheck == 0)
                 Console.WriteLine("Employee is present");
             else
                 Console.WriteLine("Employee is absent");
 
+        }
+        public void CalculateEmpWage() 
+        {
+            int dailyEmpWage = WAGE_PER_HR * FULL_DAY_HR;
+            Console.WriteLine(dailyEmpWage);
         }
     }
 }
