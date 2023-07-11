5da9df93082ebfd971de003b9cfbcda087ca16d4
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-11 10:52:56 +0530

    [Riya] Add : Implemented Employee Attendence feature

---
+++ EmployeeWage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class EmployeeWage
+    {
+        public void EmployeeAttendance()
+        {
+            Random random = new Random();
+            if empCheck = random.Next(0, 2);//returns value 0, 1
+            if (empCheck == 0)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace EmployeeWageComputation
         static void Main ( string[] args)
          {
            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            EmployeeWage employeeWage = new EmployeeWage();
+            employeeWage.EmployeeAttendance();
          }
 
     }
